1babadafc0b0d7b89418a28ab71d1051a4c33187
CHEA <heekyung4105@gmail.com>
2024-12-17 09:11:20 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.py
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.py
@@ -1,5 +1,5 @@
 def solution(array):
     answer = 0
-    array.sort()
-    answer = array[(len(array)//2)]
+    array1 = sorted(array)
+    answer = array1[len(array1)-(len(array1)//2)-1]
     return answer

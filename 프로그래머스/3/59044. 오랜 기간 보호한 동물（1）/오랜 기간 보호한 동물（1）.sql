92799ac77d0cc3af5d0c17332899bd6ce5abcfa6
CHEA <heekyung4105@gmail.com>
2024-12-18 09:06:43 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -3,5 +3,5 @@ SELECT i.NAME,
         i.DATETIME
 from ANIMAL_INS i left join ANIMAL_OUTS o on i.ANIMAL_ID=o.ANIMAL_ID
 where o.DATETIME is null
-order by datediff(now(),i.DATETIME) desc
+order by i.DATETIME 
 limit 3

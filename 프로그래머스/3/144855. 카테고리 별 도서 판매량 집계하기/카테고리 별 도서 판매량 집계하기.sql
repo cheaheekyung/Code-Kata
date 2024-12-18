31789df36fe2ea1ab7058a9a179beccc1eb8ab94
CHEA <heekyung4105@gmail.com>
2024-12-18 09:07:50 +0900

    [level 3] Title: 카테고리 별 도서 판매량 집계하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/144855. 카테고리 별 도서 판매량 집계하기/카테고리 별 도서 판매량 집계하기.sql
+++ 프로그래머스/3/144855. 카테고리 별 도서 판매량 집계하기/카테고리 별 도서 판매량 집계하기.sql
@@ -3,5 +3,5 @@ SELECT b.CATEGORY as CATEGORY,
         sum(s.SALES) as TOTAL_SALES
 from BOOK b join BOOK_SALES s on b.BOOK_ID=s.BOOK_ID
 where date_format(s.SALES_DATE,'%Y-%m')='2022-01'
-group by b.CATEGORY
+group by 1
 order by 1

52d4d51d54402f8fb49d1a3bddbdd6b19d10ff0b
CHEA <heekyung4105@gmail.com>
2024-12-24 09:29:28 +0900

    [level 2] Title: 3월에 태어난 여성 회원 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
+++ 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -1,6 +1,8 @@
 -- 코드를 입력하세요
-SELECT MEMBER_ID, MEMBER_NAME, GENDER, 
-       date_format(DATE_OF_BIRTH,'%Y-%m-%d') as DATE_OF_BIRTH
+SELECT MEMBER_ID,
+       MEMBER_NAME,
+       GENDER,
+       date_format(DATE_OF_BIRTH,'%Y-%m-%d') DATE_OF_BIRTH
 from MEMBER_PROFILE
-where month(DATE_OF_BIRTH)='03' and GENDER='w' and TLNO is not null
-order by MEMBER_ID
+where month(DATE_OF_BIRTH)=03 and GENDER = 'W' and TLNO is not null
+order by 1

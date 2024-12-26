86d12064e0411360049a363ccd501d8360b06bae
CHEA <heekyung4105@gmail.com>
2024-12-26 09:48:30 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 3, Time: 75.10 ms, Memory: 71.1 MB -BaekjoonHub

--- 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.py
+++ 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.py
@@ -1,9 +1,9 @@
 def solution(arr, k):
-    answer = []
-    if k % 2 :
-        for i in arr :
-            answer.append(i*k)
-    else :
-        for i in arr :
-            answer.append(i+k)
+    answer = [i+k if k % 2 == 0 else i*k for i in arr ]
+    # if k % 2 :
+    #     for i in arr :
+    #         answer.append(i*k)
+    # else :
+    #     for i in arr :
+    #         answer.append(i+k)
     return answer

27e8dc25720e90139bcdd5a82f0fe0a270aae680
CHEA <heekyung4105@gmail.com>
2025-01-01 19:51:49 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -1,12 +1,6 @@
 def solution(numbers):
     answer = 0
     numbers.sort()
-    if numbers[1] < 0 :
-        if numbers[0]*numbers[1] > numbers[-1]*numbers[-2] :
-            answer = numbers[0]*numbers[1]
-        else :
-            answer = numbers[-1]*numbers[-2]
-    else : 
-        answer = numbers[-1]*numbers[-2]
+    answer = max(numbers[0]*numbers[1], numbers[-1]*numbers[-2])
         
     return answer

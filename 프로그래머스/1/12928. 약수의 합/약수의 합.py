72f461a4ba65e0d5f8b9e19175d647a6dc39006d
CHEA <heekyung4105@gmail.com>
2025-01-04 13:33:00 +0900

    [level 1] Title: 약수의 합, Time: 0.16 ms, Memory: 9.98 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.py
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.py
@@ -1,6 +1,4 @@
 def solution(n):
-    answer = 0
-    for i in range(1, n+1) :
-        if n % i == 0 :
-            answer += i
+    answer = sum(i for i in range(1,n+1) if n % i == 0 )
+    
     return answer

29817156e7f9450a08beb3428589853effc73da4
CHEA <heekyung4105@gmail.com>
2025-01-07 10:05:18 +0900

    [level 1] Title: 시저 암호, Time: 1.80 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.py
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.py
@@ -1,15 +1,13 @@
 def solution(s, n) :
     answer = ''
-    ALP =['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-    alp = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    ALP = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    alp = ALP.lower()
 
     for i in s :
         if i in ALP : 
             answer += ALP[(ALP.index(i) + n) % len(ALP)]
         elif i in alp :
             answer += alp[(alp.index(i) + n) % len(alp)]
-        elif i == ' ' :
-            answer += ' '
+        else :
+            answer += i
     return answer
-
-print(solution('a B z',4))

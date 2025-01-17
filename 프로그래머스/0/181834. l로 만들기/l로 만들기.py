6937f894cc80042fba0052d55cee1ae6e0cff678
CHEA <heekyung4105@gmail.com>
2025-01-17 09:25:15 +0900

    [level 0] Title: l로 만들기, Time: 8.51 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.py
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.py
@@ -1,8 +1,7 @@
 def solution(myString):
     answer = ''
-    a = ['a','b','c','d','e','f','g','h','i','j','k']
     for i in myString :
-        if i in a :
+        if i < 'l' :
             answer += 'l'
         else :
             answer += i

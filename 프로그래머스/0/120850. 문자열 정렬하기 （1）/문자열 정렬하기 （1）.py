89b50fbc68eeb888d9e83d6f6306f37c09d58781
CHEA <heekyung4105@gmail.com>
2025-01-17 09:18:29 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
@@ -1,7 +1,6 @@
 def solution(my_string):
     answer = []
-    num = ['1','2','3','4','5','6','7','8','9','0']
     for i in my_string :
-        if i in num :
+        if i.isdigit() :
             answer.append(int(i))
     return sorted(answer)

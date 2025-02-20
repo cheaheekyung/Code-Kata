43a15299defdc526dba41abdf71da3364360477b
CHEA <heekyung4105@gmail.com>
2025-02-20 09:44:56 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 1.76 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.py
@@ -0,0 +1,9 @@
+def solution(my_string, queries):
+    
+    answer = list(my_string)
+    
+    for i,j in queries :
+        answer[i:j+1] =answer[i:j+1][::-1]
+        
+        
+    return ''.join(answer)

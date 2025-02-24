8b4b45eb3562f9687e42f94e63868c638743e725
CHEA <heekyung4105@gmail.com>
2025-02-24 09:25:22 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.py
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.py
@@ -1,9 +1,7 @@
 def solution(date1, date2):
     answer = 0
-    if date1[0] < date2[0] :
-        answer = 1
-    elif date1[0] == date2[0] and date1[1] < date2[1] :
-        answer = 1
-    elif date1[0] == date2[0] and date1[1] == date2[1] and date1[2] < date2[2] :
+    day1 =''.join(map(str, date1))
+    day2 =''.join(map(str, date2))
+    if int(day1) < int(day2) :
         answer = 1
     return answer

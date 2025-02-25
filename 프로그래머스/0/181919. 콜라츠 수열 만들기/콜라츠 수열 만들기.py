527a045af6149fa4a75941b42885c872ba1e7bcc
CHEA <heekyung4105@gmail.com>
2025-02-25 09:20:29 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.py
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.py
@@ -1,11 +1,11 @@
 def solution(n):
     answer = [n]
     while n != 1 :
-        if n % 2 == 0 :
-            answer.append(n // 2)
-            n = n // 2
-        elif n % 2 == 1 :
+        if n % 2 :
             answer.append(n*3+1)
             n = n*3+1
+        else :
+            answer.append(n // 2)
+            n = n // 2
             
     return answer

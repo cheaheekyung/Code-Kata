8b0b0ce71b5c2d1e8f84d46f09923652cd7f40f9
CHEA <heekyung4105@gmail.com>
2025-04-04 09:38:24 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.01 ms, Memory: 9.1 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.py
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.py
@@ -1,6 +1,7 @@
 def solution(my_string, s, e):
     one = my_string[:s]  
-    re = my_string[e:s-1:-1]  if s > 0 else my_string[e::-1]
+    re = my_string[s:e+1][::-1]
     two = my_string[e+1:]
     answer = one+re+two
     return answer
+

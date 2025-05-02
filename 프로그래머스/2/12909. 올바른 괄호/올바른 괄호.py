c4d002596161779ab63a541bf496db9ab06f7192
CHEA <heekyung4105@gmail.com>
2025-05-02 23:31:12 +0900

    [level 2] Title: 올바른 괄호, Time: 8.27 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,10 +1,8 @@
 def solution(s):
-    count = 0
-    for char in s:
-        if char == '(':
-            count += 1
-        else:  # char == ')'
-            count -= 1
-            if count < 0:
-                return False  # ')'가 '('보다 먼저 나옴 -> 올바르지 않음
-    return count == 0  # 모두 짝이 맞으면 0이어야 함
+    answer = True
+    o = s.count("(") 
+    c = s.count(")")
+    if s[0] == ")" or s[-1] == "(" or o != c :
+        answer = False
+
+    return answer

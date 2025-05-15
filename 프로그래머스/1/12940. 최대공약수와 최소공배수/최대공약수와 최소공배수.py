4cdfcc27ce788fe0ce6df73c8c3096704185dc68
CHEA <heekyung4105@gmail.com>
2025-05-16 00:09:59 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.62 ms, Memory: 9.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -0,0 +1,14 @@
+def solution(n, m):
+    answer = []
+    a = []
+    b = []
+    for i in range(1,n+1) :
+        if n % i == 0 and m % i == 0 :
+            a.append(i)
+    for i in range(1,m+1) :
+        if n * i % m ==0 :
+            b.append(n*i)
+    if not b :
+        b.append(n*m)
+    answer = [max(a), min(b)]
+    return answer

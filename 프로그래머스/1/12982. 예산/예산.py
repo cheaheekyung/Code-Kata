3a6d1902076a0d5d54abb10a8361775d763a2a62
CHEA <heekyung4105@gmail.com>
2025-12-16 21:56:26 +0900

    [level 1] Title: 예산, Time: 0.02 ms, Memory: 9.07 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.py
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -1,14 +1,11 @@
 def solution(d, budget):
     a = sorted(d)
     answer = 0
-    if sum(a) <= budget:
-        answer = len(a) 
-    else :
-        re = budget
-        for i in a :
-            re -= i 
-            if re < 0 :
-                break
-            else :
-                answer += 1
+    re = budget
+    for i in a :
+        re -= i 
+        if re < 0 :
+            break
+        else :
+            answer += 1
     return answer
